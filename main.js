e06140ac9fe2c9b1c8a3ef135e916950c362aa06
theunmanifested <waltersoso80@gmail.com>
2019-07-21 11:30:39 -0500

    Updated and functional website until part 2 at 15:54

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
+// nav menu drops down
 $(document).ready(function() {
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
+    });
+
+// nav menu functionality
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
     });
 });
