69d2343f2773c96e9c5cadb5afd0e912335a925b
theunmanifested <waltersoso80@gmail.com>
2019-07-24 19:43:13 -0500

    Fixed both nav menu funct and scrolling on main.js

--- main.js
+++ main.js
@@ -5,16 +5,17 @@ $(document).ready(function() {
         $('.top-nav').toggleClass('open');
     });
 
-    // // nav menu functionality
-    // $('.top-nav .nav-link').on('click', function () {
-    //     $('.menu-toggler').removeClass('open');
-    //     $('.top-nav').removeClass('open');
-    // });
+    // nav menu functionality
+    $('.top-nav .nav-link').on('click', function () {
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
 
-    // // nav menu scrolling
-    // $('nav a[href*="#"]').on('click', function () {
-    //     $('html, body').animate( keyframes: {
-    //         scrollTop: $($(this).attr('href')).offset().top - 100
-    //     }, options:2000);
-    // });
+    // nav menu scrolling
+    $('nav a[href*="#"]').on('click', function () {
+        // removed keyframes: and options: (two lines below), after .animate({})
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, 1725);
+    });
 });
