068615a7e0b3def1fc2b793bba38df0942b35c22
theunmanifested <waltersoso80@gmail.com>
2019-07-24 19:22:59 -0500

    Continued until 15:20, but only nav menu drops down js works, not menu functionality nor scrolling

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-// nav menu drops down
 $(document).ready(function() {
+    // nav menu drops down
     $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
 
-// nav menu functionality
-    $('.top-nav .nav-link').on('click', function () {
-        $('.menu-toggler').removeClass('open');
-        $('.top-nav').removeClass('open');
-    });
+    // // nav menu functionality
+    // $('.top-nav .nav-link').on('click', function () {
+    //     $('.menu-toggler').removeClass('open');
+    //     $('.top-nav').removeClass('open');
+    // });
+
+    // // nav menu scrolling
+    // $('nav a[href*="#"]').on('click', function () {
+    //     $('html, body').animate( keyframes: {
+    //         scrollTop: $($(this).attr('href')).offset().top - 100
+    //     }, options:2000);
+    // });
 });
